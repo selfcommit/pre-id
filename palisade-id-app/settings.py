4e57ebb409f137726f6c0db98bd54817194cff41
Dan OBoyle <Dan@StackOverflow.com>
2016-04-26 13:08:32 -0400

    remove 's' from palisade

---
+++ palisade-id-app/settings.py
@@ -0,0 +1,4 @@
+api_key = "AIzaSyDQumwcg4mWjthenxyYA6-8XwEA3nEDmiw"
+client_secret = "suCDhJHN8gVMKHOp3ZOaHSq3"
+client_id = "834087223792-pgbudr7q1rube2afo99ht55tsvpg08r1.apps.googleusercontent.com"
+secret = "notasecret"
